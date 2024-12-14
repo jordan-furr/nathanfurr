6b625da695824933f50967537b238a08ba38326f
jordan-furr <jordanfurr98@gmail.com>
2024-12-14 19:26:44 +0100

    home page

---
+++ component/nathan-header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="ph3 header-cont">
+        <div class="flex-row space-between block border-box align-center flex-shrink-0 flex-grow-1">
+            <div class="flex-grow-0 flex-shrink-0">
+                <a href="/" class="menu-item" exact-active-class="active">Nathan Furr</a>
+            </div>
+            <div class="flex-grow-1 flex-shrink-1 block">
+                <div class="block">
+                    <div class="header-nav-list">
+                        <div class="block">
+                            <a href="/about" class="menu-item" exact-active-class="active">About</a>
+                        </div>
+                        <div class="block">
+                            <a href="/booking" class="menu-item" exact-active-class="active">Booking</a>
+                        </div>
+                        <div class="block">
+                            <a href="/publications" class="menu-item" exact-active-class="active">Publications</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex-grow-0 flex-shrink-1">
+                <a href="/" class="menu-item" exact-active-class="active">in</a>
+            </div>
+        </div>
+    </div>
+</template>
